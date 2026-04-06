a9c44630795aaca059c0c3986df1c769138efc78
Ethicleaf123 <ravikant.ethicleaf@gmail.com>
2026-04-06 15:23:33 -0400

    Resolve error in test_operations file

--- test/test_operations.py
+++ test/test_operations.py
@@ -1,10 +1,10 @@
-from src.math_operations import add, sub
+from src.math_operations import add, subs
 
 def test_add():
     assert add(2,3) == 5
     assert add(-1, 1) == 0
 
 def test_sub():
-    assert sub(4,3) == 1
-    assert sub(5,3) == 2
-    assert sub(3,3) == 0
+    assert subs(4,3) == 1
+    assert subs(5,3) == 2
+    assert subs(3,3) == 0
