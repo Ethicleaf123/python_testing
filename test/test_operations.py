eea8bb4f43e3ed20ce1e83646e388cded15a8d51
Ethicleaf123 <ravikant.ethicleaf@gmail.com>
2026-05-19 11:14:20 -0400

    add one test cases

--- test/test_operations.py
+++ test/test_operations.py
@@ -3,6 +3,7 @@ from src.math_operations import add, subs
 def test_add():
     assert add(2,3) == 5
     assert add(-1, 1) == 0
+    assert add(3, 1) == 4
 
 def test_sub():
     assert subs(4,3) == 1
